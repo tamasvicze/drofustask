b82b8d4fe675166172901f2f689999ebd82f9f0d
Tamás Vicze <74873537+tamasvicze@users.noreply.github.com>
2024-02-13 21:52:27 +0100

    Added missing interfaces.

---
+++ drofustask1/drofustask1/Interfaces/ICreditService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drofustask1.Interfaces
+{
+    public interface ICreditService
+    {
+        void InsertMoney(int amount);
+        void RecallMoney();
+    }
+}
---
+++ drofustask1/drofustask1/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drofustask1.Interfaces
+{
+    public interface IProductService
+    {
+        void ListAllProducts();
+        void OrderProduct(string productName);
+    }
+}
